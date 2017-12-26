db8bd08097ce8e894342c591f8448e9bffda349a
Breyanna Vass <breyanna.bvass@gmail.com>
2017-12-26 19:22:22 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,6 +3,11 @@ class PigLatinize
     vowels = ["a","e","i","o","u"]
     if vowels.include?(word[0].downcase)
       word = word << "way"
+    else
+      i = 0
+      until vowels.include?(word[i].downcase) == true
+
+      end
     end
   end
 end
